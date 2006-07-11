66a7184896f0de3297a61cb3257d5843a5e5582a
Pascal MALAISE <malaise@magic.fr>
2006-07-11 19:36:02 +0000

    Add a comment explaining why this is a standalone template and not part of ada.mk.

--- units.mk
+++ units.mk
@@ -1,2 +1,5 @@
-
+# This makefile is standalone because it might be included by
+#  the client makefile in order to define units before including
+#  ada.mk
 UNITS ?= $(sort $(basename $(wildcard *.ads *.adb)))
+
