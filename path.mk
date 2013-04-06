648c416a895b5f620d6a0e4cd343b0c8eebcf76a
Pascal MALAISE <malaise@telemaque>
2013-04-06 18:19:56 +0200

    Use PCRE include

--- path.mk
+++ path.mk
@@ -3,4 +3,6 @@ CPATH     := $(ADAPATH)/c
 REPOSIT   := $(ADAPATH)/reposit
 USR       := $(ADAPATH)/usr
 GNATPATH  := /usr/local/gnat/bin
-PCRE      := `pcre-config --prefix`/lib/$(ARCH)
+PCRE_CFG  := pcre-config
+PCRE_INCL := `$(PCRE_CFG) --cflags`
+PCRE_LIB  := `$(PCRE_CFG) --prefix`/lib/$(ARCH)
