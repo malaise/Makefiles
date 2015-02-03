bbe508a7612c57681bce5a0070495164c5b16e59
Pascal MALAISE <malaise@magic.fr>
2015-02-03 16:40:54 +0100

    Fix gitignore with app

--- git.mk
+++ git.mk
@@ -9,9 +9,11 @@ git: .gitignore
 	  $(TOUCH) .gitignore; \
 	fi; \
 	$(ECHO) $(EXES) $(FILES2LINK) $(GITIGNORE) $(TEXI_TARGETS) \
-	  $(TXT_TARGETS) $(AFPX_XREF_FILE) $(CDEP) | $(SED) -e "s/ /\n/g" >> .gitignore; \
+	  $(TXT_TARGETS) $(AFPX_XREF_FILE) $(CDEP) \
+	  | $(SED) -e "s/ /\n/g" >> .gitignore; \
 	$(ECHO) $(patsubst %.aps,%.ads,$(wildcard *.aps)) \
-	         $(patsubst %.apb,%.adb,$(wildcard *.apb)) >> .gitignore;
+	         $(patsubst %.apb,%.adb,$(wildcard *.apb)) \
+	  | $(SED) -e "s/ /\n/g" >> .gitignore
 
 clean_git:
 	@$(RM) .gitignore
