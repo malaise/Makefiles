7e5eecca2c51ec511379b908f0b9675d91644135
Pascal MALAISE <malaise@magic.fr>
2015-03-24 19:55:58 +0100

    Adapt to PCRE2

--- path.mk
+++ path.mk
@@ -2,7 +2,10 @@ ADAPATH   ?= $(HOME)/ada
 CPATH     := $(ADAPATH)/c
 REPOSIT   := $(ADAPATH)/reposit
 USR       := $(ADAPATH)/usr
-GNATPATH  := /usr/local/gnat/bin
+GNATPATH  := /shared/tools/exec/gnat/bin
 PCRE_CFG  := pcre-config
 PCRE_INCL := `$(PCRE_CFG) --cflags`
 PCRE_LIB  := `$(PCRE_CFG) --prefix`/lib/$(ARCH)
+PCRE_LIBS := pcre
+PCRE_ALIBS := $(PCRE_LIBS:%=$(PCRE_LIB)/lib%.a)
+
