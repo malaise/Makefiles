d862c5183bbba525249551845d65e6e94f59fdad
Pascal MALAISE <malaise@magic.fr>
2016-05-10 15:52:30 +0200

    Use PCRE2 by default

--- path.mk
+++ path.mk
@@ -4,7 +4,7 @@ REPOSIT   := $(ADAPATH)/reposit
 USR       := $(ADAPATH)/usr
 GNATPATH  ?= /usr/local/gnat/bin
 
-PCRE ?= PCRE1
+PCRE ?= PCRE2
 CFLAGS    := -D$(PCRE)
 ifeq ($(PCRE),PCRE0)
 PCRE_CFG  := pcre-config
