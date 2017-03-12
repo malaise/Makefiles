069f425c759b3026e8d189436e99c018bc8d7d01
Pascal MALAISE <malaise@magic.fr>
2017-03-12 10:31:03 +0100

    Also exclude .gitignore

--- git.mk
+++ git.mk
@@ -8,6 +8,7 @@ git: .gitignore
 	else \
 	  $(TOUCH) .gitignore; \
 	fi; \
+  $(ECHO) ".gitignore" >> .gitignore; \
 	$(ECHO) $(PREREQS) $(EXES) $(FILES2LINK) $(GITIGNORE) $(TEXI_TARGETS) \
 	  $(TXT_TARGETS) $(AFPX_XREF_FILE) $(CDEP) \
 	  | $(SED) -e "s/ /\n/g" >> .gitignore; \
